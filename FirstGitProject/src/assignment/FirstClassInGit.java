58fa4c7318edca0d3b98044cc7218bf26079c4b6
er02990 <er02990@CEIT2553214X013.ad.georgiasouthern.edu>
2019-08-21 16:50:23 -0400

    Asked for last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,13 +1,18 @@
 
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello, my name is Evan");
 		
 		//add a statement to display your name in the console
+		String c;
+		System.out.println("What is your last name?");
+		c = sc.next();
 		
+		System.out.println("My name is Evan " + c);
 		//Then commit your changes
 
 	}
